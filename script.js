b14b88299b562eba7c673bc09af8e582c162e416
PCalvoGarcia <paulacalvogarcia@gmail.com>
2024-10-15 10:45:01 +0200

    feature: DOM select ul

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+// La lista debe cargarse automáticamente cuando la aplicación se inicia.
+// Cada ítem debe mostrarse en su propio elemento <li>, con un botón de eliminar "x" a la derecha.
+// Si la lista está vacía, no se debe mostrar ningún ítem, pero el contenedor de la lista debe permanecer visible.
+// Los ítems deben aparecer en el orden en el que fueron añadidos.
+
 // Esta lista es la que de debe mostrar en el navegador
 let items = ["Orange", "Apple", "Banana"];
-
+const shopListDOM = document.getElementById('listId')
 // Función para pintar la lista en el navegador
-function printList() {}
+function printList() {
+  console.log (shopListDOM)
+}
 
 // Función para eliminar un item de la lista
 function deleteItemFromList(item) {}
@@ -13,7 +20,7 @@ function addItemToList() {}
 // Función principal - Aquí empieza la aplicación
 function main() {
   alert("Welcome to the list app! Start deleting this alert, please.");
-  // printList();
+  printList();
 }
 
 // Llamada a la función principal
