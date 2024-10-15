dbf8c90c92fe044817dd3fc40742ad7d294887a9
PCalvoGarcia <144434991+PCalvoGarcia@users.noreply.github.com>
2024-10-15 10:03:15 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// Esta lista es la que de debe mostrar en el navegador
+let items = ["Orange", "Apple", "Banana"];
+
+// Función para pintar la lista en el navegador
+function printList() {}
+
+// Función para eliminar un item de la lista
+function deleteItemFromList(item) {}
+
+// Función para agregar un item a la lista
+function addItemToList() {}
+
+// Función principal - Aquí empieza la aplicación
+function main() {
+  alert("Welcome to the list app! Start deleting this alert, please.");
+  // printList();
+}
+
+// Llamada a la función principal
+main();
