5890855f3b85a7c100e25e418ca3b94bce535912
AlexMonzdev <ialexmonzon@gmail.com>
2024-10-16 12:01:54 +0200

    feature: validation capitalice words

--- script.js
+++ script.js
@@ -5,19 +5,15 @@
 
 // Esta lista es la que de debe mostrar en el navegador
 let items = [];
-const shopListDOM = document.getElementById('listId')
-const inputIdDOM = document.getElementById("inputId")
-const addBtnId = document.getElementById("addBtnId")
-
-
-
-
+const shopListDOM = document.getElementById("listId");
+const inputIdDOM = document.getElementById("inputId");
+const addBtnId = document.getElementById("addBtnId");
 
 // Función para pintar la lista en el navegador
 function printList() {
   shopListDOM.innerHTML = "";
-  for (let i = 0; i < items.length; i++){ 
-    shopListDOM.innerHTML += `<li>${items[i]}<span onclick="" class="item-delete-btn">x</span></li>`
+  for (let i = 0; i < items.length; i++) {
+    shopListDOM.innerHTML += `<li>${items[i]}<span onclick="" class="item-delete-btn">x</span></li>`;
   }
 }
 
@@ -29,32 +25,37 @@ function addItemToList() {
   const newItem = inputIdDOM.value.trim();
   inputIdDOM.value = "";
 
-  if(!newItem){
+  if (!newItem) {
     alert("añade algo !! ");
     return;
   }
   for (const item of items) {
-    if(item.toLowerCase() == newItem.toLowerCase()){
-     alert("ya esta en la lista");
-     return;
-   }
- }
- const itemToArray = newItem.split(" ");
- const capitalizeWords = []; 
- 
- 
- const capitaize = newItem.charAt(0).toUpperCase() + newItem.slice(1).toLowerCase();
+    if (item.toLowerCase() == newItem.toLowerCase()) {
+      alert("ya esta en la lista");
+      return;
+    }
+  }
+
+  const itemToArray = newItem.split(" ");
+  const capitalizeWords = [];
+  for (const word of words) {
+    const capitalizedWord =
+      itemToArray.charAt(0).toUpperCase() + itemToArray.slice(1).toLowerCase();
+    capitalizedWords.push(capitalizedWord);
+  }
+  capitalizedWords.join(" ");
+
+  const capitaize =
+    newItem.charAt(0).toUpperCase() + newItem.slice(1).toLowerCase();
   items.push(capitaize);
   printList();
 }
-
 
 // Función principal - Aquí empieza la aplicación
 function main() {
   // alert("Welcome to the list app! Start deleting this alert, please.");
   addBtnId.addEventListener("click", addItemToList);
   printList();
-
 }
 
 // Llamada a la función principal
